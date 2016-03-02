485f9884107b7ebef0c8ea8b07c7a06619fd6237
ryanl <lee.ryan@digitaslbi.com>
2016-03-02 17:34:33 +0000

    BrowserSync implemented

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require ('gulp'),
 
   /** Utils **/
   requiredir = require('requiredir'),
-  gulpAutoTask = require('gulp-auto-task')
+  gulpAutoTask = require('gulp-auto-task'),
+  browsersync = require('browser-sync'),
 
   /** Config **/
   paths = require('./package.json').paths;
@@ -16,4 +17,28 @@ var utils = requireDir('gulp-tasks'); // ex utils.buildJekyll
 gulpAutoTask('{*,**/*}.js', {
   base: paths.tasks,
   gulp: gulp
+});
+
+/** Helper tasks **/
+gulp.task('build', function(callback){
+    return utils.buildJekyll('callback', serve);
+});
+
+gulp.task('build:prod', function(callback){
+    return utils.buildJekyll('callback', prod);
+});
+
+gulp.task('build:assets', ['buildCss', 'buildJs', 'optimizeImg']);
+
+/** Browser sync **/
+//Init server to build directory
+gulp.task('browser', function(){
+  browserSync.init({
+    server: './' + paths.build
+  });
+});
+
+//Force reload across all devices
+gulp.task('browser:reload', function(){
+  browserSync.reload();
 });
